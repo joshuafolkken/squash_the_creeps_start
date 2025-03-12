1acf837f41e7076857ef9af3f6a75d16f805003b
joshuafolkken <joshuafolkken@gmail.com>
2025-03-12 12:23:10 +0900

    killing the player #35

--- scripts/main.gd
+++ scripts/main.gd
@@ -6,9 +6,9 @@ extends Node
 @onready var _version_label: Label = $VersionLabel
 @onready var _mob_spawn_location: PathFollow3D = $MobSpawnPath/MobSpawnLocation
 @onready var _player: CharacterBody3D = $Player
+@onready var _mob_timer: Timer = $MobTimer
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	_version_label.text = ProjectSettings.get_setting("application/config/version")
 
@@ -21,8 +21,16 @@ func _init_test_color_rect() -> void:
 	add_child(test_color_rect)
 
 
-func _on_mob_timer_timeout() -> void:
+func _spawn_mob() -> void:
 	var mob: Mob = mob_scene.instantiate()
 	_mob_spawn_location.progress_ratio = randf()
 	mob.initialize(_mob_spawn_location.position, _player.position)
 	add_child(mob)
+
+
+func _on_mob_timer_timeout() -> void:
+	_spawn_mob()
+
+
+func _on_player_hit() -> void:
+	_mob_timer.stop()
